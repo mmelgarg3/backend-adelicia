b4e88fadea75d90599235d4959e407145384815e
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-13 19:32:12 -0600

    routes, jwt for login and save session

---
+++ README.md
@@ -0,0 +1,10 @@
+## Instructions for run backend
+```bash
+cd Backend
+npm i
+npm start
+```
+
+
+## Query for create the same databse in your system
+    > In file **query.sql**
---
+++ query.sql
@@ -0,0 +1,20 @@
+-- query for create database
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` int(11),
+  `refresh_token` text DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
