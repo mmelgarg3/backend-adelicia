47df018408ff5e179900cae9b39af7b1992a050f
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-20 12:45:52 -0600

    add route for the user to see the catalog

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getUsers, Register, Login, Logout } from "../controllers/Users.js";
+import { getProducts } from "../controllers/Products.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
 
@@ -10,5 +11,9 @@ router.post('/users', Register);
 router.post('/login', Login);
 router.get('/token', refreshToken);
 router.delete('/logout', Logout);
+
+
+//routes for user
+router.get('/products', getProducts);
 
 export default router;
