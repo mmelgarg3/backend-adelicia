98254893a1b1aa32db002814a531bb839b103338
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-20 12:44:18 -0600

    create controller for show products in route <products>

---
+++ controllers/Products.js
@@ -0,0 +1,13 @@
+import Product from "../models/ProductModel.js";
+
+
+export const getProducts = async(req, res) => {
+    try {
+        const products = await Product.findAll({
+            attributes:['id','nombre','descripcion','imagen', 'precio']
+        });
+        res.json(products);
+    } catch (error) {
+        console.log(error);
+    }
+}
