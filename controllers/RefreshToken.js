b138b0903ad762ba9b5fbd8590c6b3bd4e1a906a
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-20 15:56:45 -0600

    saving name and surname in token route

--- controllers/RefreshToken.js
+++ controllers/RefreshToken.js
@@ -14,9 +14,10 @@ export const refreshToken = async(req, res) => {
         jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
             if(err) return res.sendStatus(403);
             const userId = user[0].id;
-            const name = user[0].name;
-            const email = user[0].email;
-            const accessToken = jwt.sign({userId, name, email}, process.env.ACCESS_TOKEN_SECRET,{
+            const name = user[0].nombre;
+            const surname = user[0].apellido;
+            const email = user[0].correo;
+            const accessToken = jwt.sign({userId, name, apellido,email}, process.env.ACCESS_TOKEN_SECRET,{
                 expiresIn: '15s'
             });
             res.json({ accessToken });
