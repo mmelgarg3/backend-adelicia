776be4d28643eefc145d69de6c121c31d6a24fde
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-09-27 17:37:48 -0600

    adding estate of order in insert method, in the same commit i create a new method for get all orders

--- controllers/Orders.js
+++ controllers/Orders.js
@@ -6,10 +6,22 @@ export const RegisterOrder = async(req, res) =>{
         await Order.create({
             fecha: new Date(),
             idUsuario: userId,
+            estado: 1,
             totalPedido: total
         });
         res.json({msg: "Orden realizada"});
     }catch(error){
+        console.log(error);
+    }
+}
+
+export const getOrdersForCook = async(req, res)=>{
+    try {
+        const orders = await Order.findAll({
+            attributes:['id', 'fecha', 'idUsuario', 'estado', 'totalPedido']
+        });
+        res.json(orders);
+    } catch (error) {
         console.log(error);
     }
 }
