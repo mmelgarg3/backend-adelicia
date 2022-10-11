a03750e0f9f7aee9cfc6c312bce7458a4b8e8a62
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-10-11 11:49:42 -0600

    change port

--- index.js
+++ index.js
@@ -14,9 +14,10 @@ try {
     console.error(error);
 }
 
-app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
+app.set('port', process.env.PORT || 5000)
+// app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
-app.listen(5000, ()=> console.log('Server running at port 5000'));
+app.listen(app.get('port'), ()=> console.log('Server running'));
