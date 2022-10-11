16c7bda8fb8e29c56d97f5246e5f87b6fa8afa5b
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-10-11 15:30:28 -0600

    modify sequelize connection lol

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('restauranteadelicias','root','',{
-    host: "localhost",
+const db = new Sequelize('restauranteadelicias','mmelgar','David$2020',{
+    host: "azdb6789-mysql-adelicias.mysql.database.azure.com",
     dialect: "mysql"
 });
 
