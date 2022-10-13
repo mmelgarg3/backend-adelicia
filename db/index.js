7ac15d99d41005d546fb57784a2fa729fcef42b5
mmelgarg3 <mmelgarg3@miumg.edu.gt>
2022-10-12 23:07:45 -0600

    changing to correct api

--- db/index.js
+++ db/index.js
@@ -1,21 +1,21 @@
 import mysql from "mysql";
 
 
-// const mysqlConnection = mysql.createConnection({
-//     host: 'azdb6789-mysql-adelicias.mysql.database.azure.com',
-//     user:'mmelgar',
-//     database:'restauranteadelicias',
-//     password:'David$2020',
-//     multipleStatements: true
-// });
-
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
+    host: 'azdb6789-mysql-adelicias.mysql.database.azure.com',
+    user:'mmelgar',
     database:'restauranteadelicias',
-    password:'',
+    password:'David$2020',
     multipleStatements: true
 });
+
+// const mysqlConnection = mysql.createConnection({
+//     host: 'localhost',
+//     user:'root',
+//     database:'restauranteadelicias',
+//     password:'',
+//     multipleStatements: true
+// });
 
 mysqlConnection.connect((err) =>{
     if(err){
